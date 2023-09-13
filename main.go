632f4451e380f94cb3534da647edc4b10e48ab9d
Ekene Obiasogu <ekennyobiasogu@gmail.com>
2023-09-13 08:34:00 +0100

    added documentation

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func main() {
 	// }
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
     "password=%s dbname=%s sslmode=disable",
-    "dpg-ck0c3mj6fquc73ch4log-a", 5432, "persons_414z_user", "persons_414z_user", "persons_414z")
+    "dpg-ck0c3mj6fquc73ch4log-a", 5432, "persons_414z_user", "NXghusPlovh5g49DGkBx84LwrU4h5df2", "persons_414z")
 
   db, err := sql.Open("postgres", psqlInfo)
 
