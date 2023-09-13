d965ea8e73cc5e13067db38d73eab0c4ea249d1d
Ekene Obiasogu <ekennyobiasogu@gmail.com>
2023-09-13 09:24:48 +0100

    added documentation

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 
 	"stagaTwoCrud/stageTwoHandlers"
 	//"github.com/go-chi/docgen"
@@ -16,6 +18,12 @@ import (
 )
 
 func main() {
+	// os.Setenv("DATABASE_URL", "postgres://postgres:E_kenny246810@localhost:5432/StageTwoDB")
+	// os.Setenv("DATABASE_URL", "postgres://persons_414z_user:NXghusPlovh5g49DGkBx84LwrU4h5df2@dpg-ck0c3mj6fquc73ch4log-a/persons_414z")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	  }
 	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 
 	if err != nil {
